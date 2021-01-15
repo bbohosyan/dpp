1fe3f197d542c5004f09480fb00d7d35227ebc33
siqqQ <b.bohosyan@gmail.com>
2021-01-15 18:57:53 +0200

    Initial commit

---
+++ src/test/java/com/falcon/dpp/DppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falcon.dpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

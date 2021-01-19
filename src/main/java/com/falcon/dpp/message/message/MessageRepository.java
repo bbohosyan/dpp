dab441dbaa6e74d0b0034bd5021d92a196906a93
siqqQ <b.bohosyan@gmail.com>
2021-01-19 20:46:12 +0200

    Removed unused imports in MessageRepository

--- src/main/java/com/falcon/dpp/message/message/MessageRepository.java
+++ src/main/java/com/falcon/dpp/message/message/MessageRepository.java
@@ -1,9 +1,7 @@
 package com.falcon.dpp.message.message;
 
 import com.falcon.dpp.message.message.model.Message;
-import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 public interface MessageRepository extends MongoRepository<Message, String> {
 }
